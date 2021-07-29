24ef389e0c2bf937a13f941eaffa6bbca504648c
Brett Shinnebarger <bshinnebarger@gmail.com>
2021-07-29 19:39:59 +0100

    update the meaning

--- src/whats_it_all_about.py
+++ src/whats_it_all_about.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def meaning_of_life():
     df_hmm = pd.DataFrame(data=[(2, 7), (6, 13), (11, 4), (2, 16)])
-    return df_hmm.product(axis=1).mean()
+    print(f'The answer is: {df_hmm.product(axis=1).mean()}')
